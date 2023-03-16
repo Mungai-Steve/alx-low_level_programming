9b2808a0eddf0ff422868dd4f9acee1350e2a8fe
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-16 15:41:48 -0700

    using sizeof as an operator

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program starts here
+ *
+ * sizeof - retursn the size of an expression or even as an operator
+ *
+ * Return: ends the function
+ */
+
+int main(void)
+{
+	printf("%lu", sizeof(char));
+
+	return (0);
+}
