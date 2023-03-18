0a9613d222bedda7f0bf70332572da9ac67c9fcb
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-18 05:01:21 -0700

    using putchar print lower a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - program start
+ *
+ * Return: program stop
+ */
+
+int main(void)
+
+{
+	char x = 1;
+for (x = 1; x <= 26; ++x)
+
+{
+	putchar(x);
+
+}
+	return (0);
+
+}
