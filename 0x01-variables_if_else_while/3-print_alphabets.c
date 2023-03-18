0dd54ad94b0a12ef9f73e99635619e942c732250
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-18 07:11:28 -0700

    print lower, upper alphas followed by newline

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - program starts
+ * Return: program ends
+ */
+
+int main(void)
+{
+	char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+
+{
+	putchar(ch);
+
+}
+
+for (ch = 'A'; ch <= 'Z'; ch++)
+
+{
+	putchar(ch);
+
+}
+
+{
+
+	putchar('\n');
+
+}
+	return (0);
+}
