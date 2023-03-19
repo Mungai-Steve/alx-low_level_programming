aed706aa799173f04fa0908e9af196943c26665e
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-19 01:11:32 -0700

    print all caharacters before removinq and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main- program starts
+ *
+ * description: prints the alphabet in lowercase except q & e
+ *
+ * Return: program ends
+ */
+
+int main(void)
+
+{
+	char ch;
+
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+{
+	putchar(ch);
+
+}
+{
+
+	putchar('\n');
+
+}
+	return (0);
+}
