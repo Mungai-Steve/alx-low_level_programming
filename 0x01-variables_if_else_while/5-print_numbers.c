324de957dad699625b8e97055df8a3e175f1e370
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-19 03:12:05 -0700

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program starts
+ *
+ * Return: program ends here
+ */
+
+int main(void)
+{
+
+	int num;
+
+	for (num = 0; num <= 9; num++)
+
+{
+	putchar(num);
+}
+
+{
+	putchar('\n');
+}
+
+	return (0);
+
+}
