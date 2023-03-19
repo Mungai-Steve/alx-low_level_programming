ee9f1533da0ecd63ff6cd615245825917900f36e
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-19 13:09:01 -0700

    combination of two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - program starts
+ *
+ * Return: program ends
+ */
+
+int main(void)
+
+{
+	int a, b;
+
+	for (a = '0'; a <= '8'; a++)
+
+	for (b = '1'; b <= '9'; b++)
+	
+
+{
+	putchar(a);
+	putchar(b);
+
+	if (b != '9')
+{
+	putchar(',');
+	putchar(' ');
+
+}
+}
+
+
+{
+	putchar('\n');
+
+}
+	return (0);
+
+}
