585d3771ab54d26877e591c8d93d7596a71b258c
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-19 03:25:17 -0700

    use only putchar to print 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - program starts here
+ *
+ * Return: program ends here
+ */
+
+int  main(void)
+
+{
+
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+
+{
+
+	putchar(num);
+
+}
+
+{
+
+	putchar('\n');
+
+}
+
+	return (0);
+
+}
