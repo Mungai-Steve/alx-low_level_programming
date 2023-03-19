5267706162dc18e8d193f11bb705440dc0bd7bff
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-19 03:34:36 -0700

    print z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - program starts
+ *
+ * Return: program ends here
+ */
+
+int main(void)
+
+{
+
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+
+{
+
+	putchar(ch);
+
+}
+
+{
+
+	putchar('\n');
+
+}
+
+	return (0);
+
+}
+
+
