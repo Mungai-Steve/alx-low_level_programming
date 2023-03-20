29895925b86be97f672e310b5b49723c1ad81664
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-19 23:57:29 -0700

    permutation of two digits separed by comma

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,23 +12,24 @@ int main(void)
 	int a, b;
 
 	for (a = '0'; a <= '8'; a++)
+	for (b = 1 + a; b <= '9'; b++)
 
-	for (b = '1'; b <= '9'; b++)
 	
-	if (a != b)
 {
 	putchar(a);
 	putchar(b);
 
+	if (a == '8' && b == '9')
 
+	continue;
 {
+
 	putchar(',');
 	putchar(' ');
 
 }
 }
 
-	if (a != '8' && b != '9')
 {
 	putchar('\n');
 
