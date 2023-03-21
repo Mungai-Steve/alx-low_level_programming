c100e59051825074e110dddb8cf26f035a41ca10
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-21 07:14:17 -0700

    using only the function _putchar, print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - program starts here
+ *
+ * description: use local function and print
+ *
+ * Return: program ends here
+ */
+
+int main(void)
+
+
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
