63b143f29abab4141eacc0d78be1b5a7cf2725d3
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-21 10:55:49 -0700

    fuction print lower alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - program starts here
+ *
+ * Return: program ends here
+ */
+
+void print_alphabet(void)
+{
+	
+	char c;
+	for (c = 'a'; c <= 'z'; ++c)
+{	_putchar(c);}
+{	_putchar('\n');}
+
+}
