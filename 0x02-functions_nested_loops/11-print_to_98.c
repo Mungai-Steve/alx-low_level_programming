d0eb2bf322e4b36fa95adab8fe07f8fde7b7036e
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-22 15:55:02 -0700

    write numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - print numbers from n to 98
@@ -8,11 +9,49 @@
 void print_to_98(int n)
 
 {
-	void print_to_98(int n);
-
-{
-	_putchar(n);
-	_putchar(' ');
-	n++;
-}
+	if (n <= 98)
+	{
+	for (; n <= 98; n++)
+	{
+		if (n == 98)
+	{	printf("%d", n);
+	{
+		printf("\n");
+	}
+		break;
+	}
+	
+	else
+	{
+		printf("%d, ", n);
+	{	
+		printf("\n");
+	}
+	}	
+	}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+	
+	{
+		if (n == 98)
+	
+	{
+		printf("%d", n);
+	{
+		printf("\n");
+	}
+		break;
+	}
+	
+	else
+	{	
+		printf("%d, ", n);
+	{
+		printf("\n");
+	}
+	}
+	}
+	}
 }
