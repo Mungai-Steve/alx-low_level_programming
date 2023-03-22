92ac4147c558fbd2eb8652752283630b9af07d44
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-22 01:23:47 -0700

    check if is lower i.e if (c >= 97 && c <= 122)

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks if int c is lower
+ * @c: - character to be checked
+ * Return: 1 if islower 0 if is not lower
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+{
+	return (1);
+}
+{
+	return (0);
+}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** 
+ * print_last_digit - print last digit
+ */
+
+int print_last_digit(int)
+
+	int j;
+
+	digit = j%10;
+
+	print_last_digit('%d', digit)
+
+	return (0);
+}
+
+
