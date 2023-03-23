5e043b42dfb4d73651e51a9ecaffb546758de20c
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-23 02:11:09 -0700

    if c is upper case return 1 else return 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - check if c is upper
+ * 
+ * @c: character to be checked
+ *
+ * Return: program ends
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
