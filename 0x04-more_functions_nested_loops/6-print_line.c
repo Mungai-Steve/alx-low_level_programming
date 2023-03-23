ba898e119ffcc6bad959007625e0a226126b462d
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-23 09:58:44 -0700

    draw a line in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - print line int the terminal
+ *
+ * @n: number of time '_' is goin gto be printed
+ */
+
+void print_line(int n)
+
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= n; i++)
+
+	{
+		_putchar('_');
+	}
+
+		_putchar('\n');
+
+}
