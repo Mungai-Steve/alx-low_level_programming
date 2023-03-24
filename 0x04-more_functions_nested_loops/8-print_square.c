4ab3da40ade5df23a851e51279a6e377e8e59aa9
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-24 13:08:31 -0700

    drawing a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using #
+ *
+ * size: length/width of qsquare
+ *
+ */
+
+void print_square(int size)
+
+{
+
+	int x, y;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+
+	}
+
+	for (y = 0; y < size; y++)
+	
+	{
+	
+	for (x = 0; x < size; x++)
+	
+
+	_putchar(35);
+	
+	
+	_putchar('\n');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
