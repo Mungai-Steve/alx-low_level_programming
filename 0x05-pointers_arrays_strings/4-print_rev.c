871ca5ee8722a95fdad70622a112f38859b462e5
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-27 13:29:54 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: string
+ *
+ */
+
+void print_rev(char *s)
+{
+        int i = 0;
+
+        while (s[i] != '\0')
+        {
+                i++;
+        }
+
+        while (i >= 0)
+        {
+                _putchar(s[i]);
+                i--;
+        }
+        _putchar('\n');
+
+}
