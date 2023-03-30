0325f259dae503956386e1ee8320c34af4042092
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-30 13:33:46 -0700

    strncpy function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copy string 2 to string 1
+ * @dest: destingation string
+ * @src: source string
+ * @n: max bytes to be copied
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (index = src_len; index < n; index ++)
+		dest[index] = '\0';
+	
+	return (dest);
+}
