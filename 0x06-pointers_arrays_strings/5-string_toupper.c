aa4a72044ba15326f9106ae81499ce5c8d375f59
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-30 14:14:14 -0700

    to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * string_toupper - change all lower case to upper
+ * Return: always zero
+ * @str: charcter
+ */
+char *string_toupper(char *str)
+{
+	int y = 0;
+
+	while (str[y])
+	{
+	if (str[y] >= 'a' && str[y] <= 'z')
+	str[y] -= 32;
+	y++;
+	}
+	return (0);
+}
