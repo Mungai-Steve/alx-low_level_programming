eb94572bc8d260f427ea95a7b35421933431a123
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-03-31 13:10:08 -0700

    capitalize every word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
+/**
+ * cap_string - capitalize every word
+ * Return: return capitalized string
+ * @str: string
+ */
 char *cap_string(char *str)
 {
 	int i = 0;
-	while(str[i])
+
+	while (str[i])
 	{
 	while (!(str[i] >= 'a' && str[i] <= 'z'))
 	i++;
-	
-	
-		if 
-		(str[i - 1] == ' ' ||
-	       	str[i -1] == '\t' ||
-	       	str[i - 1] == '\n' ||
-	       	str[i - 1] == ',' ||
-	       	str[i - 1] == ';' ||
-	       	str[i -1] == '.' ||
-	       	str[i - 1] == '!' ||
-	       	str[i -1] == '?' ||
-	       	str[i - 1] == '"' ||
-	       	str[i - 1] == '(' ||
-	       	str[i - 1] == ')' ||
-	       	str[i - 1] == '{' ||
-	       	str[i - 1] == '}' ||
-		i == 0)
-		str[i] -= 32;
-	       	i++;
+
+	if
+	(str[i - 1] == ' ' ||
+	str[i - 1] == '\t' ||
+	str[i - 1] ==  '\n'  ||
+	str[i - 1] == ',' ||
+	str[i - 1] == ';' ||
+	str[i - 1] ==  '.'  ||
+	str[i - 1] == '!' ||
+	str[i - 1] ==  '?'  ||
+	str[i - 1] == '"' ||
+	str[i - 1] == '(' ||
+	str[i - 1] == ')' ||
+	str[i - 1] == '{' ||
+	str[i - 1] == '}' ||
+	i == 0)
+	str[i] -= 32;
+	i++;
+
 	}
 
-	
+
 	return (str);
 }
