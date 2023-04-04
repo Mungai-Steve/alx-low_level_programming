04f414a441ce202e8ca2b013a4f339ed1687db63
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-04-04 10:57:05 -0700

    function calculate length of prefix of string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * int _strspn - function get length of prefix of a string
+ * @s: string prefix
+ * @accept: string
+ * Return: the length in bytes of the prefix
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; ++i)
+	{
+	for (j = 0; accept[j]; ++j)
+	{
+		if (s[i] == accept[j])
+		{
+		break;
+		}
+	}
+	if (!accept[j])
+	{
+	return (i);
+
+	}
+	}
+	return (i);
+}
