3089302be81c541cfde797276ad615b68595341e
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-04-08 13:03:49 -0700

    calculate the number of arguments to main

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,18 +6,17 @@
  * main - calculate the number of arguments pased to main
  * @argc: number of arguments passed to main(argument count)
  * @argv: strings/arguments to main(argument vector)
+ * (void) argv - used to escape gcc warning of unused parameter
  * Return: 0 if program is succssesful
  */
 
 int main(int argc, char **argv)
 
 {
-	
-	int i;
-	for (i = 0; i < argc; i++)
-	{
-	printf("%d\n", argc);
-	printf("%s\n", argv[i]);
-	}
+
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
 	return (0);
 }
