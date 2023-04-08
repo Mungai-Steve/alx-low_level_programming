a47f8bc5f613d65256043bb96e08f54eb2216adb
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-04-08 12:31:24 -0700

    argc calculate arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - calculate the number of arguments pased to main
+ * @argc: number of arguments passed to main(argument count)
+ * @argv: strings/arguments to main(argument vector)
+ * Return: 0 if program is succssesful
+ */
+
+int main(int argc, char **argv)
+
+{
+		printf("%d\n", argc);
+		return (0);
+}
