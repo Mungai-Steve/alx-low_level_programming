49d77af5370e48ee860da408f0bdd9330532dc5d
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-04-08 13:36:37 -0700

    program multiply two numbers passed as arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,13 +5,20 @@
  * main - multiply arguments passed to main
  * @argc: number of arguments passed to main
  * @argv: argument vector
- * Return: 0 if program is successful
+ * Return: 0 if program is successful, 1 if argc < 2
  */
 
 int main(int argc, char **argv)
 {
 	int i, j;
+
 	int k = 1;
+
+	if (argc < 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
 	for (i = 1; i < argc; i++)
 	{
 	j = atoi(argv[i]);
