0cd0a64daafc2574e033dd4cf14942d5298a106b
Mungai-Steve <stephenmungaigitau@gmail.com>
2023-04-17 12:53:40 -0700

    the macro __FILE__ expands to the name of the current input file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the name of program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main() 
+{
+    printf("%s\n", __FILE__);
+    return 0;
+}
